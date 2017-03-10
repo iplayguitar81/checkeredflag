e1f26f5a85144461d423b20abb55f669a0615e2b
iplayguitar81 <iplayguitar81@yahoo.com>
2017-03-10 04:19:39 -0800

    updating

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -107,7 +107,7 @@
 <script  src="{{URL::to(asset('/js/jquery.js'))}}"></script>
 <script src="{{URL::to(asset('/js/bootstrap.min.js'))}}"></script>
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript" src="jquery.validate.min.js"></script>
 
 
