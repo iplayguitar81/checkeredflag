55155422ae67f9665b7d7d7fc33f5ba4413f2cd2
iplayguitar81 <iplayguitar81@yahoo.com>
2017-03-22 00:56:11 -0700

    updating

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -104,7 +104,7 @@
 </div>
 
 {{--change the http to https in this once i switch it to HTTPS!!!--}}
-<script  src="{{URL::to(asset('/js/jquery.js'))}}"></script>
+{{--<script  src="{{URL::to(asset('/js/jquery.js'))}}"></script>--}}
 
 <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
 <script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
