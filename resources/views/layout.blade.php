8e40b4d009fa78b6e8b51d53e053a5fae96081e7
iplayguitar81 <iplayguitar81@yahoo.com>
2017-03-22 01:10:01 -0700

    updating

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -241,22 +241,22 @@
         <a data-role='button'  data-icon='search' data-theme="a" data-iconpos="right" onclick="codeAddress();"><span class="orangose3">Switch Location</span></a>
     </div>
     <br/>
-    <div id="wrappa">
-        <div id="loading_animation"><h3 class="contact_header">Finding locations near you...</h3><img src="loader.gif" alt="loading location"/> <img src="loader.gif" alt="loading location"/><img src="loader.gif" alt="loading location"/></div>
-        <div id="map_canvas"></div>
-        <br/>
-        <div id="sidebar" data-role="collapsible">
-        </div>
-        <br/>
-        <hr>
-        <br/>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <br/>
-        <br/>
-        <div class="fb-like-box" data-href="https://www.facebook.com/checkenginefree" data-colorscheme="dark" data-show-faces="false" data-header="true" data-stream="false" data-show-border="true"></div>
-        <br/>
-        <br/>
-    </div>
+    {{--<div id="wrappa">--}}
+        {{--<div id="loading_animation"><h3 class="contact_header">Finding locations near you...</h3><img src="loader.gif" alt="loading location"/> <img src="loader.gif" alt="loading location"/><img src="loader.gif" alt="loading location"/></div>--}}
+        {{--<div id="map_canvas"></div>--}}
+        {{--<br/>--}}
+        {{--<div id="sidebar" data-role="collapsible">--}}
+        {{--</div>--}}
+        {{--<br/>--}}
+        {{--<hr>--}}
+        {{--<br/>--}}
+        {{--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
+        {{--<br/>--}}
+        {{--<br/>--}}
+        {{--<div class="fb-like-box" data-href="https://www.facebook.com/checkenginefree" data-colorscheme="dark" data-show-faces="false" data-header="true" data-stream="false" data-show-border="true"></div>--}}
+        {{--<br/>--}}
+        {{--<br/>--}}
+    {{--</div>--}}
     <div data-role="footer" style="overflow:hidden;" data-fullscreen="true" data-theme="b" data-tap-toggle="false">
         <span class="credit">checkenginefree.com &copy; 2017</span><br/>
         <p class="disclaimer">*We make no guarantees that the location information given on this site is accurate.  If you find that there is an inaccurate location listing then please take the time to report it to us using the ''Report this as inaccurate' link.  This will help to ensure that the integrity of our data is to the benefit of yourself and other users.  If you wish to add a new location that folks are able to get their check engine light diagnosed for free then please visit the contact page in order to submit that information to us.  Once it is confirmed we will add it to our databases.  We also make no guarantees that the location listed will offer a free check engine light diagnostic.  This site makes a point of listing the chains that are famous for making such offerings.  Thanks for visiting!  Please tell your friends!  :D</p>
