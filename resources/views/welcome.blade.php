28938c71f0cf1bc4fcbbe91ee2d4d201ac984b29
iplayguitar81 <iplayguitar81@yahoo.com>
2017-03-29 20:44:29 -0700

    updating

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,6 +54,7 @@
 {{--type="text/javascript"></script>--}}
 
 {{--AIzaSyCQdVUBRQViyrdnmO6-cQnH_Xy8-wW-P6Y--}}
+<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
 <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript" src="https://maps-api-ssl.google.com/maps/api/js?sensor=false&amp;v=3&amp;libraries=geometry" ></script>
