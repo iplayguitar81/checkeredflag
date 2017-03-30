013441910a1d0ced399440e45d609eb8b173ce9c
iplayguitar81 <iplayguitar81@yahoo.com>
2017-03-29 22:06:42 -0700

    updating

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -219,7 +219,7 @@
     </div><!-- /footer -->
 </div>
 <script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
-<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript" async src="https://www.google.com/jsapi"></script>
 <script type="text/javascript" src="jquery.validate.min.js"></script>
 <script type="text/javascript" src="//code.jquery.com/mobile/1.4.3/jquery.mobile-1.4.3.min.js" ></script>
 <script type="text/javascript" src="https://maps-api-ssl.google.com/maps/api/js?sensor=false&amp;v=3&amp;libraries=geometry" ></script>
