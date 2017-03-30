f6f19832e40736026f92f8115aab6a576447791d
iplayguitar81 <iplayguitar81@yahoo.com>
2017-03-29 22:46:04 -0700

    updating

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -84,9 +84,9 @@
     (document, 'script', 'facebook-jssdk'));
 </script>
 @yield('content')
-
+<script  type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" ></script>
-<script  type="text/javascript" async  src="https://www.google.com/jsapi"></script>
+
 {{--<script  type="text/javascript"  src="https://www.gstatic.com/charts/loader.js"></script>--}}
 
 <script type="text/javascript" src="jquery.validate.min.js"></script>
